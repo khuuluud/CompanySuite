03f890f2955908f1cfbbfd255c10e55687172a69
DELL 5400 <DELL 5400@KHLUD>
2024-10-20 00:08:11 +0300

    AttachmentService

---
+++ LinkDev.CompanySutie.BLL/Common/structureServices/Attachments/AttachmentService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.CompanyBase.BLL.Common.structureServices.Attachments
+{
+    internal class AttachmentService : IAttachmentService
+    {
+
+        private readonly List<string> _allowedExtensions = new() { ".png", ".jpg", ".jpeg" };
+        private const int _allowedMaxSize = 2_097_152;
+        public bool Delete(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+                return true;
+            }
+            return false;
+        }
+
+        public string? Upload(IFormFile file, string folderName)
+        {
+            var extension = Path.GetExtension(file.FileName);
+            if (!_allowedExtensions.Contains(extension))
+                return null;
+            if (file.Length > _allowedMaxSize)
+                return null;
+
+            //var folderPath = $"{Directory.GetCurrentDirectory()}\\wwwroot\\files\\{folderName}";
+
+            var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\files", folderName);
+            if(!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            var fileName = $"{Guid.NewGuid()}{extension}"; //must be unique
+
+            var filePath = Path.Combine(folderPath, fileName); // file location placed
+
+            //streaming => data per time
+          using  var fileStream = new FileStream(filePath, FileMode.Create);
+          
+            file.CopyTo(fileStream);
+            return fileName;
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
---
+++ LinkDev.CompanySutie.BLL/Common/structureServices/Attachments/IAttachmentService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.CompanyBase.BLL.Common.structureServices.Attachments
+{
+    internal interface IAttachmentService
+    {
+
+        string Upload(IFormFile file, string folderName);
+
+        bool Delete(string filePath);
+
+
+
+
+
+    }
+}
